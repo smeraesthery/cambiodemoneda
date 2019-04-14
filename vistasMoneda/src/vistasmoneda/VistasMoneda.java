01af5f86059beba96b07efb8e96633c567c8d0d0
yajayra <msalazaresthery@crece.uss.edu.pe>
2019-04-14 11:22:05 -0500

    intento de subida 1

--- vistasMoneda/src/vistasmoneda/VistasMoneda.java
+++ vistasMoneda/src/vistasmoneda/VistasMoneda.java
@@ -6,7 +6,9 @@ public class VistasMoneda {
 
     
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("*****************");
+	System.out.println("nuevo intento");
+	System.out.println("Abel no grabes o te borro todo");
     }
     
 }
